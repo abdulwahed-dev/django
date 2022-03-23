61bd90ed8e50310d148a93360eb08ba4fbeee5da
Abdulwahed Dev <abdulwahed.dev@gmail.com>
2022-03-23 19:04:05 +0200

    LoginForm

--- user_management/users/urls.py
+++ user_management/users/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
-from .views import home
 
+from django.urls import path
+from .views import home, RegisterView  # Import the view here
 
 urlpatterns = [
     path('', home, name='users-home'),
-    
+    path('register/', RegisterView.as_view(), name='users-register'),  # This is what we added
 ]
