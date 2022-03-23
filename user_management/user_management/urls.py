f104c90fc3dd94e868a841234d5585b9ce5a61d7
Abdulwahed Dev <abdulwahed.dev@gmail.com>
2022-03-23 20:05:35 +0200

    SOCIAL AUTH GITHUB

--- user_management/user_management/urls.py
+++ user_management/user_management/urls.py
@@ -7,6 +7,8 @@ from django.contrib.auth import views as auth_views
 from users.views import CustomLoginView  
 from users.forms import LoginForm
 
+from django.conf.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,4 +17,7 @@ urlpatterns = [
     path('login/', CustomLoginView.as_view(redirect_authenticated_user=True, template_name='users/login.html',
                                            authentication_form=LoginForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+
+    # add this new url entry to include the social auth's urls
+    url(r'^oauth/', include('social_django.urls', namespace='social')),
 ]
