ed7f1fa9da7bab11e1d6f953384f0f9da289fc60
Abdulwahed Dev <abdulwahed.dev@gmail.com>
2022-03-23 19:40:17 +0200

    social_django

--- user_management/user_management/urls.py
+++ user_management/user_management/urls.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
+# Add the following to the list of previous imports
+from django.contrib.auth import views as auth_views
+from users.views import CustomLoginView  
+from users.forms import LoginForm
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),  # This is what we added
+    # Add this path
+    path('login/', CustomLoginView.as_view(redirect_authenticated_user=True, template_name='users/login.html',
+                                           authentication_form=LoginForm), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
--- user_management/users/views.py
+++ user_management/users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect 
 from django.contrib import messages
 from django.views import View 
-from .forms import RegisterForm
+
+from django.contrib.auth.views import LoginView
+from .forms import RegisterForm, LoginForm
 
 
 def home(request):
@@ -12,6 +14,14 @@ class RegisterView(View):
     form_class = RegisterForm
     initial = {'key': 'value'}
     template_name = 'users/register.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        # will redirect to the home page if a user tries to access the register page while logged in
+        if request.user.is_authenticated:
+            return redirect(to='/')
+
+        # else process dispatch as it otherwise normally would
+        return super(RegisterView, self).dispatch(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
         form = self.form_class(initial=self.initial)
@@ -29,3 +39,23 @@ class RegisterView(View):
             return redirect(to='/')
 
         return render(request, self.template_name, {'form': form})
+
+
+
+
+# Class based view that extends from the built in login view to add a remember me functionality
+class CustomLoginView(LoginView):
+    form_class = LoginForm
+
+    def form_valid(self, form):
+        remember_me = form.cleaned_data.get('remember_me')
+
+        if not remember_me:
+            # set session expiry to 0 seconds. So it will automatically close the session after the browser is closed.
+            self.request.session.set_expiry(0)
+
+            # Set session as modified to force data updates/cookie to be saved.
+            self.request.session.modified = True
+
+        # else browser session will be as long as the session cookie time "SESSION_COOKIE_AGE" defined in settings.py
+        return super(CustomLoginView, self).form_valid(form)
